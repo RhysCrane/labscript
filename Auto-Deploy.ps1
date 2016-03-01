a9d387583ca33ae78d1550c9e7ba8892fe001a87
rhyscrane <rhys.crane87@gmail.com>
2016-03-01 09:39:13 +0000

    add dhcp and auto deploy scripts to work on

---
+++ Auto-Deploy.ps1
@@ -0,0 +1,7 @@
+﻿#Deploy new VMS from within vnet??? (puppet, chef better options??)
+#WDS server (script) - access to host storage (known vm template setting)
+
+
+#execute script on host - via ip ---- before setup?? hard
+#clone vm (script + vbox) > prep vm with prep script (??) > add to domain using script (??) > managable state
+
