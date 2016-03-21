a9dd1d7f71398fb0877cc9d8747df62909000bdf
RhysCrane <Rhys.crane87@gmail.com>
2016-03-21 09:40:08 +0000

    Delete Auto-Deploy.ps1

--- Auto-Deploy.ps1
+++
@@ -1,7 +0,0 @@
-﻿#Deploy new VMS from within vnet??? (puppet, chef better options??)
-#WDS server (script) - access to host storage (known vm template setting)
-
-
-#execute script on host - via ip ---- before setup?? hard
-#clone vm (script + vbox) > prep vm with prep script (??) > add to domain using script (??) > managable state
-
