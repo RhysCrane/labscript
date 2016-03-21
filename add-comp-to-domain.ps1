137f3fd9a89d84ac2a477ae0ef0a7d81ef931a66
RhysCrane <Rhys.crane87@gmail.com>
2016-03-21 09:40:37 +0000

    Delete add-comp-to-domain.ps1

--- add-comp-to-domain.ps1
+++
@@ -1,5 +0,0 @@
-﻿#join server to domain 
-
-$domain = Read-Host('Enter domain Name')
-Add-Computer -DomainName $domain -Credential (Get-Credential) -Restart -Verbose
-#if successful - restart-computer
